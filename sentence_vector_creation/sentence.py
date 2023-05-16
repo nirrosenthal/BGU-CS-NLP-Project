52cfe5c5c3a6e05adbe35eb4253419ca839e7f49
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2023-05-16 18:26:39 +0300

    update draft interface for Word and Sentence format to be used to create vectors

---
+++ sentence_vector_creation/sentence.py
@@ -0,0 +1,12 @@
+class Sentence:
+    def __init__(self, raw_data):
+        # bible/hebrew
+        self._source
+        # sentence id
+        self._id = "Tanah.Prophets.Joshua.1.1"
+        # len of sentence
+        self._len = 0
+        # index of hifeil object
+        self._hifeil_index = 9
+        # list of Word objects
+        self._words: [Word] = []
---
+++ sentence_vector_creation/word.py
@@ -0,0 +1,85 @@
+class Word:
+    def __init__(self, raw_data, source):
+        self._raw_data = raw_data
+        # set default values-
+        self._gender = 0
+        self._number = 0
+        # times
+        self._time_past = 1
+        self._time_present = 0
+        self._time_future = 0
+        # word type
+        self._adp = 0
+        self._verb = 0
+        self._noun = 0
+        self._name = 0
+        self._adjective = 0
+        # person type
+        self._first_person = 0
+        self._second_person = 0
+        self._third_person = 0
+        # set values based on type of raw data
+        if source == "Bible":
+            self.set_bible_values()
+        elif source == "Modern":
+            self.set_modern_hebrew_values()
+        else:
+            raise TypeError
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def time_past(self):
+        return self._time_past
+
+    @property
+    def time_present(self):
+        return self._time_present
+
+    @property
+    def time_future(self):
+        return self._time_future
+
+    @property
+    def adp(self):
+        return self._adp
+
+    @property
+    def verb(self):
+        return self._verb
+
+    @property
+    def noun(self):
+        return self._noun
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def adjective(self):
+        return self._adjective
+
+    @property
+    def first_person(self):
+        return self._first_person
+
+    @property
+    def second_person(self):
+        return self._second_person
+
+    @property
+    def third_person(self):
+        return self._third_person
+
+    def set_bible_values(self):
+        raise NotImplementedError
+
+    def set_modern_hebrew_values(self):
+        raise NotImplementedError
