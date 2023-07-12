c825cc81a83b69a0d256def860ef76fc63473691
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2023-07-12 20:40:02 +0300

    match both modern hebrew and bible datasets, and create unified Word object (added several WordTypes + a few others)

--- sentence_vector_creation/word.py
+++ sentence_vector_creation/word.py
@@ -4,6 +4,7 @@ from enum import Enum
 class Number(Enum):
     SINGULAR = 1
     PLURAL = 2
+    DUAL = 3
     NONE = 0
 
 
@@ -24,6 +25,7 @@ class Person(Enum):
     FIRST = 1
     SECOND = 2
     THIRD = 3
+    ANY = 4
     NONE = 0
 
 
@@ -33,17 +35,91 @@ class WordType(Enum):
     VERB = 3
     ADJ = 4
     CONJ = 5
+    NOUN = 6
+    DET = 7
+    ADV = 8
+    AUX = 9
+    PRON = 10
+
+def find_gender(male, female):
+    if male == 1:
+        return Gender.MALE
+    elif female == 1:
+        return Gender.FEMALE
+    else:
+        return Gender.NONE
+
+
+def find_number(singular, plural, dual):
+    if singular == 1:
+        return Number.SINGULAR
+    elif plural == 1:
+        return Number.PLURAL
+    elif dual == 1:
+        return Number.DUAL
+    else:
+        return Number.NONE
+
+
+def find_tense(past, present, future):
+    if past == 1:
+        return Tense.PAST
+    elif present == 1:
+        return Tense.PRESENT
+    elif future == 1:
+        return Tense.FUTURE
+    else:
+        return Tense.NONE
+
+
+def find_word_type(adp, propn, verb, adj, conj, noun, det, adv, aux, pron):
+    if adp:
+        return WordType.ADP
+    elif propn:
+        return WordType.PROPN
+    elif verb:
+        return WordType.VERB
+    elif adj:
+        return WordType.ADJ
+    elif conj:
+        return WordType.CONJ
+    elif noun:
+        return WordType.NOUN
+    elif det:
+        return WordType.DET
+    elif adv:
+        return WordType.ADV
+    elif aux:
+        return WordType.AUX
+    elif pron:
+        return WordType.PRON
+    else:
+        raise TypeError
+
+
+
+def find_person(first, second, third, _any):
+    if first == 1:
+        return Person.FIRST
+    elif second == 1:
+        return Person.SECOND
+    elif third == 1:
+        return Person.THIRD
+    elif _any == 1:
+        return Person.ANY
+    else:
+        return Person.NONE
 
 
 class Word:
-    def __init__(self, source: str, word: str, gender: Gender, number: Number, tense: Tense,
-                 word_type: WordType, person: Person):
+    def __init__(self, source: str, word: str, gender: Gender, number: Number, tense: Tense, word_type: WordType, person: Person):
         self._source = source
         self._word = word
         self._male = 1 if gender == Gender.MALE else 0
         self._female = 1 if gender == Gender.FEMALE else 0
         self._singular = 1 if number == Number.SINGULAR else 0
         self._plural = 1 if number == Number.PLURAL else 0
+        self._dual = 1 if number == Number.DUAL else 0
         self._tense_past = 1 if tense == Tense.PAST else 0
         self._tense_present = 1 if tense == Tense.PRESENT else 0
         self._tense_future = 1 if tense == Tense.FUTURE else 0
@@ -52,66 +128,72 @@ class Word:
         self._adj = 1 if word_type == WordType.ADJ else 0
         self._conj = 1 if word_type == WordType.CONJ else 0
         self._propn = 1 if word_type == WordType.PROPN else 0
+        self._pron = 1 if word_type == WordType.PRON else 0
+        self._noun = 1 if word_type == WordType.NOUN else 0
+        self._det = 1 if word_type == WordType.DET else 0
+        self._adv = 1 if word_type == WordType.ADV else 0
+        self._aux = 1 if word_type == WordType.AUX else 0
         self._first_person = 1 if person == Person.FIRST else 0
         self._second_person = 1 if person == Person.SECOND else 0
-        self._third_person = 1 if person == Person.Third else 0
+        self._third_person = 1 if person == Person.THIRD else 0
+        self._any = 1 if person == Person.ANY else 0
 
-    @property
-    def male(self):
-        return self._male
-
-    @property
-    def female(self):
-        return self._female
-
-    @property
-    def singular(self):
-        return self._singular
-
-    @property
-    def plural(self):
-        return self._plural
-
-    @property
-    def time_past(self):
-        return self._tense_past
-
-    @property
-    def time_present(self):
-        return self._tense_present
-
-    @property
-    def time_future(self):
-        return self._tense_future
-
-    @property
-    def adpositional_phrase(self):
-        return self._adp
-
-    @property
-    def verb(self):
-        return self._verb
-
-    @property
-    def proper_name(self):
-        return self._propn
-
-    @property
-    def adjective(self):
-        return self._adj
-
-    @property
-    def conjunction(self):
-        return self._conj
-
-    @property
-    def first_person(self):
-        return self._first_person
-
-    @property
-    def second_person(self):
-        return self._second_person
-
-    @property
-    def third_person(self):
-        return self._third_person
+    # @property
+    # def male(self):
+    #     return self._male
+    #
+    # @property
+    # def female(self):
+    #     return self._female
+    #
+    # @property
+    # def singular(self):
+    #     return self._singular
+    #
+    # @property
+    # def plural(self):
+    #     return self._plural
+    #
+    # @property
+    # def time_past(self):
+    #     return self._tense_past
+    #
+    # @property
+    # def time_present(self):
+    #     return self._tense_present
+    #
+    # @property
+    # def time_future(self):
+    #     return self._tense_future
+    #
+    # @property
+    # def adpositional_phrase(self):
+    #     return self._adp
+    #
+    # @property
+    # def verb(self):
+    #     return self._verb
+    #
+    # @property
+    # def proper_name(self):
+    #     return self._propn
+    #
+    # @property
+    # def adjective(self):
+    #     return self._adj
+    #
+    # @property
+    # def conjunction(self):
+    #     return self._conj
+    #
+    # @property
+    # def first_person(self):
+    #     return self._first_person
+    #
+    # @property
+    # def second_person(self):
+    #     return self._second_person
+    #
+    # @property
+    # def third_person(self):
+    #     return self._third_person
